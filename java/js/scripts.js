ef1ea3dbea62f0d9c9b58e7db4966bbbc89a81df
maeloisaaa <mecs3@aluno.ifnmg.edu.br>
2025-06-10 19:43:12 +0000

    m

--- java/js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const sportsBtn = document.getElementById("sports-btn");
-  const sportsMenu = document.getElementById("sports-menu");
-
-  sportsBtn.addEventListener("click", () => {
-    sportsMenu.classList.toggle("show");
-  });
-});
